48224dc399037d4b76eae1e1b2fc9311284a1c73
ServersLLC <36006892+ServersLLC@users.noreply.github.com>
2018-02-07 15:05:29 +0300

    Updating Installer
    Script for installing VestaCP

---
+++ install.sh
@@ -0,0 +1,4 @@
+#Modified by serversllc.Com (SERVERSLLC Hosting)
+
+curl -O http://vestacp.com/pub/vst-install.sh
+bash vst-install.sh --nginx yes --apache yes --phpfpm no --named no --remi no --vsftpd yes --proftpd no --iptables yes --fail2ban yes --quota no --exim yes --dovecot no --spamassassin no --clamav no --softaculous yes --mysql yes --postgresql no
